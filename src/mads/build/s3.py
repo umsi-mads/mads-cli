44017a4332f470e65d25210daf7f785024d510bf
Violet Shreve <shreve@umich.edu>
2024-07-22 14:28:53 -0400

    Improve S3 find_keys searching

--- src/mads/build/s3.py
+++ src/mads/build/s3.py
@@ -6,13 +6,24 @@ import boto3
 def find_keys(
     bucket: str,
     prefix: str = "",
-    contains: list[str] = [""],
+    contains: list[str] = [],
 ) -> list[str]:
     list_objects = boto3.client("s3").get_paginator("list_objects_v2")
     paginator = list_objects.paginate(Bucket=bucket, Prefix=prefix)
-    queries = [f"contains(Key, '{term}')" for term in contains]
-    search = f"Contents[?{' && '.join(queries)}].Key"
-    results = paginator.search(search)
+
+    results = []
+
+    if contains and not isinstance(contains, list):
+        contains = [contains]
+
+    if contains:
+        queries = [f"contains(Key, '{term}')" for term in contains]
+        search = f"Contents[?{' && '.join(queries)}].Key"
+        results = paginator.search(search)
+    else:
+        results = [
+            entry.get("Key") for page in paginator for entry in page.get("Contents", [])
+        ]
 
     return list(results)
 
@@ -20,7 +31,7 @@ def find_keys(
 def find_key(
     bucket: str,
     prefix: str = "",
-    contains: list[str] = [""],
+    contains: list[str] = [],
 ) -> str | None:
     for key in find_keys(bucket, prefix, contains):
         return key
